060fd787c80319c5c1d9a045c898703c85a121b2
Bryan Astacio <B.Astacio@motorcredito.com.do>
2019-03-08 11:49:19 -0400

    loading angular with webpack

--- app.module.js
+++ app.module.js
@@ -1,12 +1,21 @@
+// importing modules
+import a from './node_modules/angular/angular';
+
+// importing Components
 import homeComponent from "./component/home/home.component";
 
 var app = angular.module('app', []);
 
+//configs
+
+// constants
+
+// values
+
 // components
 app.component('homeComponent', homeComponent);
-
 
 //services
 
 
-//el diablo
+
